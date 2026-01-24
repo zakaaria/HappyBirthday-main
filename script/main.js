58107fb4368a6ad34e07416ce1e74a5150013f23
zakaria abrare <151097035+zakaaria@users.noreply.github.com>
2026-01-24 18:04:11 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -342,7 +342,7 @@ document.addEventListener("DOMContentLoaded", () => {
   
   // Configuration
   const CONFIG = {
-   targetDate: new Date("January 24, 2026 23:00:00").getTime(),
+    targetDate: new Date("January 24, 2026 23:00:00").getTime(),
     updateInterval: 1000,
     elements: {
       countdown: null,
